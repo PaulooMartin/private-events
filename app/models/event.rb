5363b51186b23cf17764594145d643b27bbee420
PaulooMartin <paulmartinblopez@gmail.com>
2023-12-12 09:40:40 +0800

    Add form & page for creating & displaying event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,10 @@
 class Event < ApplicationRecord
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User',
               inverse_of: 'hosted_events'
+
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :datetime, comparison: { greater_than_or_equal_to: DateTime.now,
+            message: "must be greater than the current date and time"}
 end
